b14a95da1bbdf79c001404375616c9089a77d6a9
brickatyourfeet <brickatyourfeet@gmail.com>
2018-02-09 12:01:00 -0800

    important commit message

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let thumb = document.querySelector('.thumb')
 
 thumb.addEventListener('click', (e) => {
   if(!thumb.classList.contains('up') && !thumb.classList.contains('sideways')) {
+    console.log('Do you seriously have the console open?')
+    //thanks for a brutal and amazing six months. -Kyle Braden
     thumb.classList.add('up')
   } else if (thumb.classList.contains('up')) {
     thumb.classList.remove('up')
